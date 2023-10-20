78b2ac6b76415700bbb4072e14503110e9dcbd3a
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-10-20 13:11:53 +0800

    chore: change it to game

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,8 +5,8 @@
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/greet/game.tsx";
-import * as $4 from "./routes/greet/mime.tsx";
+import * as $3 from "./routes/game/index.tsx";
+import * as $4 from "./routes/game/mime.tsx";
 import * as $5 from "./routes/index.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 import * as $$1 from "./islands/Square.tsx";
@@ -16,8 +16,8 @@ const manifest = {
     "./routes/_404.tsx": $0,
     "./routes/_app.tsx": $1,
     "./routes/api/joke.ts": $2,
-    "./routes/greet/game.tsx": $3,
-    "./routes/greet/mime.tsx": $4,
+    "./routes/game/index.tsx": $3,
+    "./routes/game/mime.tsx": $4,
     "./routes/index.tsx": $5,
   },
   islands: {
